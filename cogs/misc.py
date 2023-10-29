9fca7bc1c737b106f91814d45a24b0e13e3c2b17
alkine <AllucityH@gmail.com>
2023-10-29 15:15:16 +0000

    trying to fix the activity_status command

--- cogs/misc.py
+++ cogs/misc.py
@@ -20,37 +20,38 @@ class misc(commands.Cog):
 
     @commands.command()
     async def activity_stats(self, ctx):
-        print('Command ran!')
-        offline_247=0
-        barely_active=0
-        active_degree=0
-        active=0
-        dangerously_active=0
+        """
+        okay im sorry but im not amazing at python so i don't know how to use cases/switches, sorry!
+        """
         member_count = ctx.guild.member_count
+        count=[0,0,0,0,0,0]
 
         for member in ctx.guild.members:
             if member.bot != True:
-                if member.get_role(self.activity_roles[0]) != None:
-                    offline_247+=1
-                if member.get_role(self.activity_roles[1]) != None:
-                    barely_active+=1
-                if member.get_role(self.activity_roles[2]) != None:
-                    active_degree+=1
-                if member.get_role(self.activity_roles[3]) != None:
-                    active+=1
-                if member.get_role(self.activity_roles[4]) != None:
-                    dangerously_active+=1
+                roles = member.roles
+                for role in roles:
+                    if role.id==self.activity_roles[0]:
+                        count[0]+=1
+                    elif role.id==self.activity_roles[1]:
+                        count[1]+=1
+                    elif role.id==self.activity_roles[2]:
+                        count[2]+=1
+                    elif role.id==self.activity_roles[3]:
+                        count[3]+=1
+                    elif role.id==self.activity_roles[4]:
+                        count[4]+=1
+                    else:
+                        count[5]+=1
 
-        print(f"""
-        **BETA COMMAND, THIS MAY NOT WORK!**
-        Server Members: {member_count}
-        Percentages:
-            Offline_24/7: {(member_count/offline_247)*100}
-            Barely_Active: {(member_count/barely_active)*100}
-            Active To A Degree: {(member_count/active_degree)*100}
-            Active: {(member_count/active)*100}
-            Dangerously Active: {(member_count/dangerously_active)*100}
-        """)
+        await ctx.send(f"Offline 24/7: {(member_count/count[0])*100}%\nBarely Active: {(member_count/count[1])*100}%\nSort of Active: {(member_count/count[2])*100}%\nActive: {(member_count/count[3])*100}%\nDangerously Active: {(member_count/count[4])*100}%")
+
+
+        
+                
+
+        
+        
+
 
 
 
