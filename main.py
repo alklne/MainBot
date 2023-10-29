9a87e2be49e6ecbd54b30ad85ae64d96c22571e0
alkine <AllucityH@gmail.com>
2023-10-29 15:25:08 +0000

    main.py more readable removed the activity_status command

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+"""
+Hello!
+I'm very, very bad at python- I'm also new to git, so please point out how I could improve my code overall through the issues
+tab.
+
+Thanks!
+"""
+
 import settings
 import discord
 import os
 from discord.ext import commands
-
-def wipeScreen():
-    os.system('cls')
 
 def run():
     intents = discord.Intents.default()
@@ -15,49 +20,44 @@ def run():
 
     @bot.event
     async def on_ready():
-        """ Load cogs here, and make the show UI, etc """
-
-        wipeScreen()
-
+        """
+        The area where cogs are loaded.
+        """
         print("""                                                     
-          .---.                                               
-          |   |     .     .--.   _..._         __.....__      
-          |   |   .'|     |__| .'     '.   .-''         '.    
-          |   | .'  |     .--..   .-.   . /     .-''"'-.  `.  
-    __    |   |<    |     |  ||  '   '  |/     /________\   \ 
- .:--.'.  |   | |   | ____|  ||  |   |  ||                  | 
-/ |   \ | |   | |   | \ .'|  ||  |   |  |\    .-------------' 
-`" __ | | |   | |   |/  . |  ||  |   |  | \    '-.____...---. 
- .'.''| | |   | |    /\  \|__||  |   |  |  `.             .'  
-/ /   | |_'---' |   |  \  \   |  |   |  |    `''-...... -'    
-\ \._,\ '/      '    \  \  \  |  |   |  |                     
- `--'  `"      '------'  '---''--'   '--'                     
+                  .---.                                               
+                  |   |     .     .--.   _..._         __.....__      
+                  |   |   .'|     |__| .'     '.   .-''         '.    
+                  |   | .'  |     .--..   .-.   . /     .-''"'-.  `.  
+            __    |   |<    |     |  ||  '   '  |/     /________\   \ 
+         .:--.'.  |   | |   | ____|  ||  |   |  ||                  | 
+        / |   \ | |   | |   | \ .'|  ||  |   |  |\    .-------------' 
+        `" __ | | |   | |   |/  . |  ||  |   |  | \    '-.____...---. 
+         .'.''| | |   | |    /\  \|__||  |   |  |  `.             .'  
+        / /   | |_'---' |   |  \  \   |  |   |  |    `''-...... -'    
+        \ \._,\ '/      '    \  \  \  |  |   |  |                     
+         `--'  `"      '------'  '---''--'   '--'                     
         """)
 
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
-                print(f"Loading COG {filename}")
                 try:
                     await bot.load_extension(f'cogs.{filename[:-3]}')
                 except:
-                    print(f"FAILED TO LOAD COG {filename}")
+                    print(f"Failed to load: {filename}")
                 else:
-                    print(f"LOADED COG {filename}")
+                    print(f"Loaded: {filename}")
 
         print(f"\n\nLoaded!\nUsername: {bot.user}\nID: {bot.user.id}")
 
     @bot.event
     async def on_command_error(ctx, error):
-        """ Error handler """
-
         if isinstance(error, commands.CommandNotFound):
             await ctx.send("**Command not found**", delete_after=5)
             return
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("**You can't run this command, as you don't have the permissions to**", delete_after=5)
             return
-
-
+        
     bot.run(settings.DISCORD_API_SECRET)
 
 if __name__ == "__main__":
